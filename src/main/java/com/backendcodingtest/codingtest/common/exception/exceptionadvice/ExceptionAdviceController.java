45d5f0ba4c6302d98055d1b4dd45d24fffc2c633
jyh4358 <jyh4358@gmail.com>
2022-08-06 18:02:25 +0900

    feat: ExceptionAdviceController 구현

---
+++ src/main/java/com/backendcodingtest/codingtest/common/exception/exceptionadvice/ExceptionAdviceController.java
@@ -0,0 +1,54 @@
+package com.backendcodingtest.codingtest.common.exception.exceptionadvice;
+
+import com.backendcodingtest.codingtest.common.exception.ErrorResponse;
+import com.backendcodingtest.codingtest.common.exception.httpexception.ExistException;
+import com.backendcodingtest.codingtest.common.exception.httpexception.NotFountException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class)
+public class ExceptionAdviceController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.warn("MethodArgumentNotValidException {}", exception.getMessage(), exception);
+        BindingResult result = exception.getBindingResult();
+        return ErrorResponse.error(HttpStatus.BAD_REQUEST.value(), result.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList()));
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ErrorResponse handleException(Exception exception) {
+        log.error("Exception/RuntimeException {}", exception.getMessage(), exception);
+        return ErrorResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ExistException.class)
+    public ErrorResponse handleExistException(ExistException e) {
+        log.error(e.getClass() + ": " + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()));
+        return ErrorResponse.error(e.getCode(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFountException.class)
+    public ErrorResponse handleNotFountException(NotFountException e) {
+        log.error(e.getClass() + ": " + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()));
+        return ErrorResponse.error(e.getCode(), e.getMessage());
+    }
+
+}
